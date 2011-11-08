8ead29f4d9d6b6d3bf9843e08a97b457c300442a
dcrelling <david_crelling@hotmail.com>
2011-11-08 07:06:27 -0500

    nada

--- src/com/burningman/ArtList.java
+++ src/com/burningman/ArtList.java
@@ -51,34 +51,12 @@ public class ArtList extends ListActivity {
     helper.consumeRestService(ArtList.ART_URL, this.getBaseContext());
   }
   
-  
-
   /** Called when the activity is first created. */
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     getConvertRequestFromDB();
-    
-
-    
-    
-    /*
-    String artRequestValue = getArtRequestFromDB();
-    if(artRequestValue != null){
-      convertToArtList(artRequestValue);
-    }else{ 
-      HttpServiceHelper helper =  new HttpServiceHelper();
-      helper.consumeRestService(ArtList.ART_URL, this.getBaseContext());
-    }
-    */
-    
-    //HttpProvider httpProvider = new HttpProvider();
-    //convertToArtList(httpProvider.getHttpContent(ART_URL, this));
-    
     setContentView(R.layout.expressionlist);
-    //expressionListAdapter = new ExpressionListAdapter(this, R.layout.listrow, artList);
-    //setListAdapter(expressionListAdapter);
-
     ListView artListView = getListView();
     artListView.setOnItemClickListener(new OnItemClickListener() {
       @Override
@@ -90,66 +68,11 @@ public class ArtList extends ListActivity {
     });
 
   }
-/*
-  private void convertToArtList(String page) {
-    artList = new ArrayList<Expression>();
-    try {
-      // A Simple JSONArray Creation
-      JSONArray jsonArtArray = new JSONArray(page);
-      // A Simple JSONObject Parsing
-      Art art = null;
-      for (int i = 0; i < jsonArtArray.length(); i++) {
-        art = new Art();
-        JSONObject jsonArtObject = (JSONObject) jsonArtArray.get(i);
-        art.setArtist(jsonArtObject.optString("artist"));
-        art.setCircular_street(jsonArtObject.optString("circular_street"));
-        art.setContact_email(jsonArtObject.optString("contact_email"));
-        art.setDescription(jsonArtObject.optString("description"));
-        art.setId(jsonArtObject.optString("id"));
-        art.setName(jsonArtObject.optString("name"));
-        art.setSlug(jsonArtObject.optString("slug"));
-        art.setUrl(jsonArtObject.optString("url"));
-        artList.add(art);
-        /*
-         * JSONArray nameArray = json_art_object.names(); JSONArray valArray =json_art_object.toJSONArray(nameArray);
-         * for(int j=0; j<valArray.length(); j++) {
-         * Log.i("Praeda","<jsonname"+j+">\n"+nameArray.getString(j)+"\n</jsonname"+j+">\n"
-         * +"<jsonvalue"+j+">\n"+valArray.getString(j)+"\n</jsonvalue"+j+">"); }
-         */ 
-  /*
-      }
-      // A Simple JSONObject Value Pushing
-      // json.put("sample key", "sample value");
-      // Log.i("Praeda","<jsonobject>\n"+json.toString()+"\n</jsonobject>");
-    } catch (JSONException e) {
-      // TODO Auto-generated catch block
-    }
-  } */
-  
-  /*
-  public String getArtRequestFromDB(){
-    BurningmanDBAdapter dbAdapter = new BurningmanDBAdapter(this);
-    dbAdapter.open();
-    Cursor ArtListCursor = dbAdapter.getRestRequests(ArtList.TAG);
-    if (ArtListCursor != null) {
-      if (ArtListCursor.getCount() > 0) {
-        ArtListCursor.moveToFirst();
-        String artRequestValue = ArtListCursor.getString(ArtListCursor
-            .getColumnIndex(RestRequestMetaData.REST_REQUEST_VALUE));
-        dbAdapter.close();
-        dbAdapter = null;
-        return artRequestValue;
-      }
-    }
-    dbAdapter.close();
-    dbAdapter = null;
-    return null;
-  } */
-  
+
   private void getConvertRequestFromDB(){
-    DBServiceHelper dBQueryHelper = new DBServiceHelper();
-    dBQueryHelper.registerCallBackHandler(myHandler);
-    dBQueryHelper.executeOperation(ArtList.TAG, this.getBaseContext(), "getConvertRequest");
+    DBServiceHelper dBServiceHelper = new DBServiceHelper();
+    dBServiceHelper.registerCallBackHandler(myHandler);
+    dBServiceHelper.executeOperation(ArtList.TAG, this.getBaseContext(), "getConvertRequest");
   }
   
 }
