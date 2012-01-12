e7e1b7c98ae4324589e5d0d46c5410bf54d2a64e
dcrelling <david_crelling@hotmail.com>
2012-01-12 15:42:26 -0500

    added custom exceptions

---
+++ src/com/burningman/exception/DBException.java
@@ -0,0 +1,15 @@
+package com.burningman.exception;
+
+public class DBException extends Exception {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  public DBException(String message)
+  {
+    super(message);
+  }
+  
+}
---
+++ src/com/burningman/exception/HTTPException.java
@@ -0,0 +1,14 @@
+package com.burningman.exception;
+
+public class HTTPException extends Exception {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  public HTTPException(String message)
+  {
+    super(message);
+  }
+}
